77cdc6a139107632281e4a3111a413ed1f3d3977
Amel <zekafrombtm@hotmail.com>
2020-04-06 14:35:38 +0200

    Updates

---
+++ precache-manifest.82ed6e7dc35f22da14df75a3bcdaf370.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ccc13ba7b35d8fd36bb4868f9c434e9",
+    "url": "/ToDoList-onlyReact/index.html"
+  },
+  {
+    "revision": "7be6fe3e3a70cb6b2df3",
+    "url": "/ToDoList-onlyReact/static/css/main.775a7ccc.chunk.css"
+  },
+  {
+    "revision": "56c84f7e9d6c44538c2f",
+    "url": "/ToDoList-onlyReact/static/js/2.a335fa8e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ToDoList-onlyReact/static/js/2.a335fa8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7be6fe3e3a70cb6b2df3",
+    "url": "/ToDoList-onlyReact/static/js/main.44d481ac.chunk.js"
+  },
+  {
+    "revision": "c03edb42b89186a230fe",
+    "url": "/ToDoList-onlyReact/static/js/runtime-main.2d394a4b.js"
+  },
+  {
+    "revision": "7372ec3b87b91f35c9ca0ab02f41ddc1",
+    "url": "/ToDoList-onlyReact/static/media/setnja.7372ec3b.JPG"
+  }
+]);
